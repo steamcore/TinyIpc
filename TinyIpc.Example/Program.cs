28db3ed19e45e3c5c5d2a86386f812eba200276c
David Zidar <david.zidar@gmail.com>
2014-01-30 12:52:56 +0100

    Added example project

---
+++ TinyIpc.Example/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace TinyIpc.Example
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			using (var messagebus = new TinyMessageBus("Example"))
+			{
+				messagebus.MessageReceived += (sender, e) => Console.WriteLine(e.Message);
+
+				while (true)
+				{
+					var message = Console.ReadLine();
+					messagebus.PublishAsync(message);
+				}
+			}
+		}
+	}
+}
