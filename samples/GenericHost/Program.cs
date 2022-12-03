fd1be40d9962c41c1939e3f52480d7a4a08fa483
David Zidar <david@zidar.se>
2022-12-04 00:29:09 +0100

    Remove pragma warning disable CA1812

--- samples/GenericHost/Program.cs
+++ samples/GenericHost/Program.cs
@@ -1,4 +1,3 @@
-#pragma warning disable CA1812 // Workaround for analyzer bug https://github.com/dotnet/roslyn-analyzers/issues/5628
 using GenericHost;
 
 var host = Host.CreateDefaultBuilder(args)
